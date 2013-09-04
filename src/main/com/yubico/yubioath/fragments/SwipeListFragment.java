b928a634245fb42400c47fd6ef59737dc743c08c
Dain Nilsson <dain@yubico.com>
2013-09-04 16:20:01 +0200

    Added instructions for adding the support library to maven.

--- src/main/com/yubico/yubioath/fragments/SwipeListFragment.java
+++ src/main/com/yubico/yubioath/fragments/SwipeListFragment.java
@@ -5,7 +5,6 @@ import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
