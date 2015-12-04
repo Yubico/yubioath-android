8260c1b904bc7f682d576dad698733ba41215eb1
Dain Nilsson <dain@yubico.com>
2015-12-04 15:56:52 +0100

    Restructured project to be more AndroidStudio friendly.

---
+++ app/proguard-rules.pro
@@ -0,0 +1,27 @@
+# Add project specific ProGuard rules here.
+# By default, the flags in this file are appended to flags specified
+# in C:\Users\Dain\AppData\Local\Android\sdk/tools/proguard/proguard-android.txt
+# You can edit the include path and order by changing the proguardFiles
+# directive in build.gradle.
+#
+# For more details, see
+#   http://developer.android.com/guide/developing/tools/proguard.html
+
+# Add any project specific keep options here:
+
+# If your project uses WebView with JS, uncomment the following
+# and specify the fully qualified class name to the JavaScript interface
+# class:
+#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
+#   public *;
+#}
+
+# The Nordpol library contains methods that will be invoked by the
+# system and will therefore be removed by proguard. This forces
+# proguard to keep those methods.
+-keep class nordpol.** { *; }
+
+# The Nordpol library contains references to newer platform versions.
+# Don't warn about those in case this app is linking against an older
+# platform version.  We know about them, and they are safe.
+-dontwarn nordpol.android.**
---
+++ app/src/main/java/com/yubico/yubioath/fragments/SwipeListFragment.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2013, Yubico AB.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+package com.yubico.yubioath.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.yubico.yubioath.MainActivity;
+import com.yubico.yubioath.R;
+import com.yubico.yubioath.model.KeyManager;
+import com.yubico.yubioath.model.YubiKeyNeo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dain
+ * Date: 9/4/13
+ * Time: 3:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SwipeListFragment extends Fragment implements MainActivity.OnYubiKeyNeoListener {
+    private ListCodesFragment fragment = new ListCodesFragment();
+    private DummyListCodesFragment emptyFragment1 = new DummyListCodesFragment();
+    private DummyListCodesFragment emptyFragment2 = new DummyListCodesFragment();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.swipe_list_fragment, container, false);
+        final ViewPager pager = (ViewPager) view.findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(0);
+        pager.setAdapter(new ListAdapter(getFragmentManager()));
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (state == 0 && pager.getCurrentItem() != 1) {
+                    fragment.showCodes(new ArrayList<Map<String, String>>());
+                    pager.setCurrentItem(1, false);
+                }
+
+            }
+        });
+
+        pager.setCurrentItem(1, false);
+
+        return view;
+    }
+
+    @Override
+    public void onYubiKeyNeo(YubiKeyNeo neo) throws IOException {
+        fragment.onYubiKeyNeo(neo);
+    }
+
+    @Override
+    public void onPasswordMissing(KeyManager keyManager, byte[] id, boolean missing) {
+        fragment.onPasswordMissing(keyManager, id, missing);
+    }
+
+    public ListCodesFragment getCurrent() {
+        return fragment;
+    }
+
+    private class ListAdapter extends FragmentStatePagerAdapter {
+        public ListAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return emptyFragment1;
+                case 1:
+                    return fragment;
+                default:
+                    return emptyFragment2;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }
+}
