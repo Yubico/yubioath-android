63387c02a39be5d22e688aa9305a54eed1f40916
Dain Nilsson <dain@yubico.com>
2015-12-09 09:51:56 +0100

    Fixed tests.

--- app/src/test/java/com/yubico/yubioath/fragments/AddAccountFragmentTest.java
+++ app/src/test/java/com/yubico/yubioath/fragments/AddAccountFragmentTest.java
@@ -33,13 +33,17 @@ package com.yubico.yubioath.fragments;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertEquals;
 
 import android.net.Uri;
 
-@RunWith(RobolectricTestRunner.class)
+import com.yubico.yubioath.BuildConfig;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
 public class AddAccountFragmentTest {
 	AddAccountFragment fragment;
 	
