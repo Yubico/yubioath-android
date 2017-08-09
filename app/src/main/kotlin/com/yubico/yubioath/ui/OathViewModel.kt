6f39fc073c55ad90953aa32ed2965ba126c09b2a
Dain Nilsson <dain@yubico.com>
2017-08-09 10:24:01 +0200

    Improve USB permission prompting.

--- app/src/main/kotlin/com/yubico/yubioath/ui/OathViewModel.kt
+++ app/src/main/kotlin/com/yubico/yubioath/ui/OathViewModel.kt
@@ -2,8 +2,11 @@ package com.yubico.yubioath.ui
 
 import android.app.PendingIntent
 import android.arch.lifecycle.ViewModel
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import android.util.Log
 import com.yubico.yubioath.client.Code
@@ -40,6 +43,8 @@ class OathViewModel : ViewModel() {
 
     private val lock = Mutex()
     private var services: Services? = null
+    private var usbReceiver : BroadcastReceiver? = null
+    private val devicesPrompted: MutableSet<UsbDevice> = mutableSetOf()
     private val clientRequests = Channel<Pair<ByteArray, (OathClient) -> Unit>>()
 
     var lastDeviceInfo = DUMMY_INFO
@@ -56,7 +61,27 @@ class OathViewModel : ViewModel() {
     fun start(context: Context) = launch(EXEC) {
         lock.withLock {
             val keyManager = KeyManager(context.getSharedPreferences(KEY_STORE, Context.MODE_PRIVATE))
-            services = Services(context, context.usbManager, keyManager)
+            services = Services(context, context.usbManager, keyManager).apply {
+                val filter = IntentFilter(ACTION_USB_PERMISSION)
+                usbReceiver = object : BroadcastReceiver() {
+                    override fun onReceive(context: Context, intent: Intent) {
+                        val device = intent.getParcelableExtra<UsbDevice>(UsbManager.EXTRA_DEVICE)
+                        if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                            Log.d("yubioath", "USB permission granted, $device")
+                            if (device != null) {
+                                launch(EXEC) {
+                                    lock.withLock {
+                                        useBackend(UsbBackend.connect(usbManager, device), keyManager)
+                                    }
+                                }
+                            }
+                        } else {
+                            Log.d("yubioath", "USB denied!")
+                        }
+                    }
+                }
+                context.registerReceiver(usbReceiver, filter)
+            }
             Log.d("yubioath", "Started!")
             updateRefreshJob()
         }
@@ -64,7 +89,9 @@ class OathViewModel : ViewModel() {
 
     fun stop() = launch(EXEC) {
         lock.withLock {
+            services?.apply { context.unregisterReceiver(usbReceiver) }
             services = null
+            usbReceiver = null
             refreshJob?.cancel()
             refreshJob = null
             Log.d("yubioath", "Stopped!")
@@ -115,6 +142,7 @@ class OathViewModel : ViewModel() {
 
     fun clearCredentials() {
         creds.clear()
+        selectedItem = null
         credListener(creds)
         updateRefreshJob()
     }
@@ -132,29 +160,33 @@ class OathViewModel : ViewModel() {
                 } else {
                     val now = System.currentTimeMillis() //Needs to use real time, not adjusted for non-persistent.
                     val deadline = creds.filterKeys { it.type == OathType.TOTP && !it.touch }.values.map { it?.validUntil ?: -1 }.filter { it > now }.min()
-                    if(deadline != null) deadline - now else 1000L
+                    if (deadline != null) deadline - now else 1000L
                 })
             }
         }
     }
 
     private suspend fun checkUsb(services: Services) {
-        services.usbManager.deviceList.values.find { UsbBackend.isSupported(it) }.let {
-            if(it == null) {
-                if(lastDeviceInfo.persistent) {
-                    Log.d("yubioath", "Persistent device removed!")
-                    lastDeviceInfo = DUMMY_INFO
+        val device = services.usbManager.deviceList.values.find { UsbBackend.isSupported(it) }
+        if (device == null) {
+            if (lastDeviceInfo.persistent) {
+                Log.d("yubioath", "Persistent device removed!")
+                lastDeviceInfo = DUMMY_INFO
+            }
+        } else {
+            if (services.usbManager.hasPermission(device)) {
+                Log.d("yubioath", "USB device present")
+                lock.withLock {
+                    useBackend(UsbBackend.connect(services.usbManager, device), services.keyManager)
                 }
+            } else if (device in devicesPrompted) {
+                Log.d("yubioath", "USB no permission, already requested!")
             } else {
-                if (services.usbManager.hasPermission(it)) {
-                    Log.d("yubioath", "USB device present")
-                    lock.withLock {
-                        useBackend(UsbBackend.connect(services.usbManager, it), services.keyManager)
-                    }
-                } else {
-                    val mPermissionIntent = PendingIntent.getBroadcast(services.context, 0, Intent(ACTION_USB_PERMISSION), 0)
-                    services.usbManager.requestPermission(it, mPermissionIntent)
-                }
+                Log.d("yubioath", "USB no permission, request")
+                devicesPrompted.add(device)
+                val mPermissionIntent = PendingIntent.getBroadcast(services.context, 0, Intent(ACTION_USB_PERMISSION), 0)
+                services.usbManager.requestPermission(device, mPermissionIntent)
+                clearCredentials()
             }
         }
     }
