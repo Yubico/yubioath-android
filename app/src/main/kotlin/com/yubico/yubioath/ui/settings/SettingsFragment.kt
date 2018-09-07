50dc0e0b05be96ac284b4a06bc593dd01074c97e
Dain Nilsson <dain@yubico.com>
2018-09-07 16:18:21 +0200

    Tidy up preferencesa bit.

--- app/src/main/kotlin/com/yubico/yubioath/ui/settings/SettingsFragment.kt
+++ app/src/main/kotlin/com/yubico/yubioath/ui/settings/SettingsFragment.kt
@@ -1,51 +1,55 @@
 package com.yubico.yubioath.ui.settings
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
-import androidx.preference.ListPreference
-import androidx.preference.Preference
-import androidx.preference.PreferenceFragmentCompat
 import android.text.Html
 import android.text.method.LinkMovementMethod
 import android.view.WindowManager
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.ViewModelProviders
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 import com.yubico.yubioath.R
 import com.yubico.yubioath.scancode.KeyboardLayout
 
 class SettingsFragment : PreferenceFragmentCompat() {
     private val viewModel: SettingsViewModel by lazy { ViewModelProviders.of(activity!!).get(SettingsViewModel::class.java) }
 
+    private inline fun onPreferenceChange(key: String, crossinline func: (value: Any) -> Unit) {
+        preferenceManager.findPreference(key).onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, value ->
+            func(value)
+            true
+        }
+    }
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences)
 
         activity?.apply {
-            preferenceManager.findPreference("hideThumbnail").onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, hide ->
-                window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, if (hide == true) WindowManager.LayoutParams.FLAG_SECURE else 0)
-                true
+            onPreferenceChange("hideThumbnail") {
+                window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, if (it == true) WindowManager.LayoutParams.FLAG_SECURE else 0)
             }
 
-            preferenceManager.findPreference("clearIcons").onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            onPreferenceChange("clearIcons") {
                 AlertDialog.Builder(this)
                         .setTitle(R.string.clear_icons)
                         .setMessage(R.string.clear_icons_message)
                         .setPositiveButton(R.string.clear) { _, _ -> viewModel.clearIcons() }
                         .setNegativeButton(R.string.cancel, null)
                         .show()
-                true
             }
 
-            preferenceManager.findPreference("clearPasswords").onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            onPreferenceChange("clearPasswords") {
                 AlertDialog.Builder(this)
                         .setTitle(R.string.clear_passwords)
                         .setMessage(R.string.clear_passwords_message)
                         .setPositiveButton(R.string.clear) { _, _ -> viewModel.clearStoredPasswords() }
                         .setNegativeButton(R.string.cancel, null)
                         .show()
-                true
             }
 
-            preferenceManager.findPreference("about").onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            onPreferenceChange("about") { _ ->
                 val appVersion: String = packageManager.getPackageInfo(packageName, 0).versionName
                 val oathVersion = with(viewModel.lastDeviceInfo) { if (id.isNotEmpty()) "$version" else getString(com.yubico.yubioath.R.string.no_device) }
 
@@ -53,22 +57,25 @@ class SettingsFragment : PreferenceFragmentCompat() {
                         .setTitle(R.string.about_title)
                         .setMessage(Html.fromHtml(String.format(getString(R.string.about_text), appVersion, oathVersion)))
                         .create().apply {
-                    show()
-                    findViewById<TextView>(android.R.id.message)?.movementMethod = LinkMovementMethod.getInstance()
-                    findViewById<TextView>(android.R.id.message)?.movementMethod = LinkMovementMethod.getInstance()
-                    viewModel.onDeviceRefresh = {
-                        if(isShowing) {
-                            setMessage(Html.fromHtml(String.format(getString(R.string.about_text), appVersion, it.version)))
+                            show()
+                            findViewById<TextView>(android.R.id.message)?.movementMethod = LinkMovementMethod.getInstance()
+                            findViewById<TextView>(android.R.id.message)?.movementMethod = LinkMovementMethod.getInstance()
+                            viewModel.onDeviceRefresh = {
+                                if (isShowing) {
+                                    setMessage(Html.fromHtml(String.format(getString(R.string.about_text), appVersion, it.version)))
+                                }
+                            }
                         }
-                    }
-                }
+            }
 
-                true
+            onPreferenceChange("readNdefData") {
+                preferenceManager.findPreference("keyboardLayout").isEnabled = it == true
             }
 
             (preferenceManager.findPreference("keyboardLayout") as ListPreference).apply {
                 entryValues = KeyboardLayout.availableLayouts
                 entries = entryValues
+                isEnabled = preferenceManager.sharedPreferences.getBoolean("readNdefData", false)
             }
         }
     }
